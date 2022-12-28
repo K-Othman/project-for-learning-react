4b532b9c1f250740ee682bf431d969a52f68dba8
K-Othman <karim_othman88@hotmail.com>
2022-12-28 21:49:02 +0000

    adding useEffect and fetching the data

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,9 +1,17 @@
-import productsData from "../Products-data";
 import Products from "./Products";
 import "./ProductList.css";
+import { useEffect, useState } from "react";
 
 function ProductsList() {
-  const prodcutList = productsData.map((product) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  const prodcutList = products.map((product) => {
     return product.price > 100 ? (
       <Products key={product.id} product={product} />
     ) : null;
